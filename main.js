cd471a768f569325d9e0e12cb3189da660a43342
VaiPyro <88515547+VaiPyro@users.noreply.github.com>
2021-08-05 17:52:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function spanele(){
+window.alert ("This is a span element")
+document.getElementById("tune").play();
+}
+function divele(){
+window.alert ("This is a div element")
+document.getElementById("tune").play();
+}
